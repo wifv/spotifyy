9422b916c1c14be4e1b0a0c96e9ecd29f2fbc252
wifv <abcogenshin@gmail.com>
2024-01-06 19:54:12 +0500

    adsf

--- js/script.js
+++ js/script.js
@@ -2,13 +2,22 @@ const blockElement = '' +
     '<div class="music-block pointer">\n' +
     '    <img src="../images/error.svg" alt="" class="music-image">\n' +
     '    <p>ambatukam</p>\n' +
-    '    <div class="play">play</div>' +
+    '    <img class="play" src="./images/play.svg"></img>' +
     '    <audio class="hidden"></audio>' +
     '</div>';
 
 const blocks = document.getElementsByClassName('music-block');
 const blocksContainer = document.getElementById('block-container');
 const musicTrack = document.getElementById("music-track");
+const lol = document.getElementById("lol");
+const lolImage = document.getElementById("lol-image");
+const volume = document.getElementById("volume");
+const slider = document.getElementById("slider");
+const durationElement = document.getElementById("duration-wrapper");
+const cTime = document.getElementById("cTime");
+let duration;
+
+volume.volume = 1;
 
 fetch('https://test.imowww.uz/api/music/track/', {
     method: "GET"
@@ -28,19 +37,19 @@ fetch('https://test.imowww.uz/api/music/track/', {
         }
         blocksContainer.children[i].children[1].innerText = data[i].name;
         blocksContainer.children[i].children[3].src = data[i].music_file;
-        // blocksContainer.children[i].children[3].src = new Audio(data[i].music_file)
+
         blocksContainer.children[i].children[2].addEventListener("click", () => {
-            musicTrack.src = blocksContainer.children[i].children[3].src;
-            musicTrack.paused = false
-            if(musicTrack.paused == true) {
-                console.log(musicTrack.paused)
-                musicTrack.play();
-                console.log(musicTrack.paused)
-            } else if (musicTrack.paused == false) {
-                musicTrack.pause();
+            if(musicTrack.src != blocksContainer.children[i].children[3].src) {
+                musicTrack.src = blocksContainer.children[i].children[3].src;
             }
+            slider.min = 0;
+            slider.max = Math.round(musicTrack.duration);
+            durationElement.innerText = Math.round(musicTrack.duration);
+            playPause(musicTrack);
+            console.log(musicTrack.duration)
         })
     }
+    return data;
 })
 
 // fetch('https://test.imowww.uz/api/music/track/', {
@@ -57,3 +66,42 @@ fetch('https://test.imowww.uz/api/music/track/', {
 //         music_file: "http://test.imowww.uz/media/track/2023/12/23/%D0%A5%D0%B0%D0%B1%D0%B8%D0%B1_-_%D0%AF%D0%B3%D0%BE%D0%B4%D0%B0_%D0%9C%D0%B0%D0%BB%D0%B8%D0%BD%D0%BA%D0%B0_eAAnc2I.mp3",
 //     })
 // })
+
+
+
+
+volume.addEventListener('change', () => {
+    musicTrack.volume = volume.value / 100;
+})
+
+slider.addEventListener('change', () => {
+    musicTrack.currentTime = slider.value;
+    cTime.innerText = slider.value;
+})
+
+musicTrack.addEventListener("timeupdate", () => {
+    slider.value = musicTrack.currentTime;
+    cTime.innerText = slider.value;
+})
+
+document.body.addEventListener('keyup', key => {
+    if(key.key == " ") {
+        console.log(key.key)
+        playPause(musicTrack)
+        durationElement.innerText = Math.round(musicTrack.duration);
+    }
+})
+
+function playPause(musicTrack) {
+    if(musicTrack.paused == true) {
+        musicTrack.play();
+    } else if (musicTrack.paused == false) {
+        musicTrack.pause();
+    }
+}
+
+lolImage.addEventListener("click", () => {
+    console.log('negr');
+    playPause(lol)
+    console.log('negr')
+})
